f257dde0babe0c944b9377b16dd40a8e9b3e2f11
Miguel Victor <miguelvictor.remulta@outlook.com>
2020-10-24 13:59:38 +0800

    Initial Commit

---
+++ card-matcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  card-matcher
+//
+//  Created by Miguel Victor on 10/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ card-matcher/card_matcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_matcherApp.swift
+//  card-matcher
+//
+//  Created by Miguel Victor on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct card_matcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
